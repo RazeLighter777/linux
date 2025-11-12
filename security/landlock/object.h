fd7ba15ca3731d57bcbffafedc3c01b636399f46
Justin Suess <utilityemal77@gmail.com>
2025-11-12 12:47:24 -0500

    caching xarray lookup

--- security/landlock/object.h
+++ security/landlock/object.h
@@ -13,6 +13,8 @@
 #include <linux/refcount.h>
 #include <linux/spinlock.h>
 
+#include "access.h"
+
 struct landlock_object;
 
 /**
@@ -60,6 +62,14 @@ struct landlock_object {
 	 * by @lock.  Cf. landlock_release_inodes() and release_inode().
 	 */
 	void *underobj;
+	/**
+	 * @no_inherit_desc_layers: Cache of layers for which a descendant rule
+	 * carries the no_inherit flag. This is a union of desc_layers from all
+	 * rulesets' xarray entries for this object. Protected by @lock.
+	 * This cache prevents xarray lookups on the hot path in
+	 * landlock_get_no_inherit_desc_layers().
+	 */
+	layer_mask_t no_inherit_desc_layers;
 	union {
 		/**
 		 * @rcu_free: Enables lockless use of @usage, @lock and
