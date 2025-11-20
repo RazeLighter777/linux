60e4a9b8eb7afa29e021e2564431875d44ed67f3
Justin Suess <utilityemal77@gmail.com>
2025-11-20 14:04:27 -0500

    landlock: Implement LANDLOCK_ADD_RULE_NO_INHERIT userspace api
    Implements the syscall side flag handling and kernel api headers for the
    LANDLOCK_ADD_RULE_NO_INHERIT flag.
    
    Signed-off-by: Justin Suess <utilityemal77@gmail.com>

--- include/uapi/linux/landlock.h
+++ include/uapi/linux/landlock.h
@@ -127,10 +127,19 @@ struct landlock_ruleset_attr {
  *     allowed_access in the passed in rule_attr.  When this flag is
  *     present, the caller is also allowed to pass in an empty
  *     allowed_access.
+ * %LANDLOCK_ADD_RULE_NO_INHERIT
+ *     When this flag is set while adding a rule to a ruleset, the rule
+ *     will not inherit allowed accesses from rules on parent objects
+ *     within the same layer. (currently only applies to filesystem objects)
+ *     By default, Landlock rules added to a ruleset inherit allowed accesses
+ *     from parent objects, meaning that if a parent directory has been granted
+ *     certain access rights, those rights will also apply to its child objects.
+ *     This flag prevents such inheritance for the specific rule being added.
  */
 
 /* clang-format off */
 #define LANDLOCK_ADD_RULE_QUIET			(1U << 0)
+#define LANDLOCK_ADD_RULE_NO_INHERIT		(1U << 1)
 /* clang-format on */
 
 /**
