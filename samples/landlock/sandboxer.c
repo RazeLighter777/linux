0a3e489a8ad774a9313ee08ab317d4cf12c1fd89
Justin Suess <utilityemal77@gmail.com>
2025-12-21 14:42:58 -0500

    samples/landlock: Add LANDLOCK_ADD_RULE_NO_INHERIT to landlock-sandboxer
    Adds support to landlock-sandboxer with environment variable
    LL_FS_NO_INHERIT, which can be tagged on any filesystem object to
    suppress access right inheritance.
    
    Cc: Tingmao Wang <m@maowtm.org>
    Signed-off-by: Justin Suess <utilityemal77@gmail.com>

--- samples/landlock/sandboxer.c
+++ samples/landlock/sandboxer.c
@@ -60,6 +60,7 @@ static inline int landlock_restrict_self(const int ruleset_fd,
 #define ENV_FS_RW_NAME "LL_FS_RW"
 #define ENV_FS_QUIET_NAME "LL_FS_QUIET"
 #define ENV_FS_QUIET_ACCESS_NAME "LL_FS_QUIET_ACCESS"
+#define ENV_FS_NO_INHERIT_NAME "LL_FS_NO_INHERIT"
 #define ENV_TCP_BIND_NAME "LL_TCP_BIND"
 #define ENV_TCP_CONNECT_NAME "LL_TCP_CONNECT"
 #define ENV_NET_QUIET_NAME "LL_NET_QUIET"
@@ -400,6 +401,7 @@ static const char help[] =
 	"but to test audit we can set " ENV_FORCE_LOG_NAME "=1\n"
 	ENV_FS_QUIET_NAME " and " ENV_NET_QUIET_NAME ", both optional, can then be used "
 	"to make access to some denied paths or network ports not trigger audit logging.\n"
+	ENV_FS_NO_INHERIT_NAME " can be used to suppress access right propagation (ABI >= 8).\n"
 	ENV_FS_QUIET_ACCESS_NAME " and " ENV_NET_QUIET_ACCESS_NAME " can be used to specify "
 	"which accesses should be quieted (defaults to all):\n"
 	"* " ENV_FS_QUIET_ACCESS_NAME ": file system accesses to quiet\n"
@@ -453,6 +455,7 @@ int main(const int argc, char *const argv[], char *const *const envp)
 	};
 
 	bool quiet_supported = true;
+	bool no_inherit_supported = true;
 	int supported_restrict_flags = LANDLOCK_RESTRICT_SELF_LOG_NEW_EXEC_ON;
 	int set_restrict_flags = 0;
 
@@ -553,6 +556,7 @@ int main(const int argc, char *const argv[], char *const *const envp)
 	case 9:
 		/* Don't add quiet flags for ABI < 9 later on. */
 		quiet_supported = false;
+		no_inherit_supported = false;
 
 		__attribute__((fallthrough));
 	case LANDLOCK_ABI_LAST:
@@ -657,6 +661,13 @@ int main(const int argc, char *const argv[], char *const *const envp)
 	if (quiet_supported && getenv(ENV_FS_QUIET_NAME)) {
 		if (populate_ruleset_fs(ENV_FS_QUIET_NAME, ruleset_fd, 0,
 					LANDLOCK_ADD_RULE_QUIET))
+			goto err_close_ruleset;
+	}
+
+	/* Don't require this env to be present. */
+	if (no_inherit_supported && getenv(ENV_FS_NO_INHERIT_NAME)) {
+		if (populate_ruleset_fs(ENV_FS_NO_INHERIT_NAME, ruleset_fd, 0,
+					LANDLOCK_ADD_RULE_NO_INHERIT))
 			goto err_close_ruleset;
 	}
 
